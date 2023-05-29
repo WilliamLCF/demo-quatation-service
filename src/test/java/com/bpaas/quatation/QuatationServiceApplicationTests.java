d7917adfaa2f3ef9760bf9c11c068c0b6540b59d
William <williaml@tw.ibm.com>
2023-05-29 21:44:17 +0800

    remove test

--- src/test/java/com/bpaas/quatation/QuatationServiceApplicationTests.java
+++ src/test/java/com/bpaas/quatation/QuatationServiceApplicationTests.java
@@ -3,10 +3,10 @@ package com.bpaas.quatation;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class QuatationServiceApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
